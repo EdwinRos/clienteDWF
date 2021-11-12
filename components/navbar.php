ec4dd83767d4613a383639cf358ac2a400c3af1c
EdwinRos <e.edwirosales@gmail.com>
2021-11-11 18:29:24 -0600

    xd

--- components/navbar.php
+++ components/navbar.php
@@ -1,21 +1,21 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <span class="navbar-brand">Paciente</span>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center w-90">
-                <li class="nav-item">
-                    <span class="nav-link active">Es un gusto atenderle</span>
-                </li>
-            </ul>
-            <form action="../controllers/registro.php" method="post">
+        <div class="container-fluid">
+            <span class="navbar-brand">Paciente</span>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center w-90">
+                    <li class="nav-item">
+                        <span class="nav-link active">Es un gusto atenderle</span>
+                    </li>
+                </ul>
+                <form action="../controllers/registro.php" method="post">
                     <span class="navbar-text">
                         <input type="hidden" name="action" value="logOut">
                         <button type="submit" class="btn btn-primary">Cerrar sesion</button>
                     </span>
-            </form>
+                </form>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
