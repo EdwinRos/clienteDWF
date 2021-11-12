767ec2421768b640d5fdc2c3ce8c0178a1d08690
Emerson21983 <55989868+Emerson21983@users.noreply.github.com>
2021-11-12 13:09:49 -0600

    detalles del paciente terminado

--- views/detallesPaciente.php
+++ views/detallesPaciente.php
@@ -27,59 +27,91 @@
             width: 90%;
         }
     </style>
+    <link rel="stylesheet" href="../static/css/cards-paciente.css">
 <body>
 <?php
 include "../components/navbar.php"
 ?>
     <div class="container">
         <div class="row">
-            <div class="col-md-12 mt-5">
+            <div class="col-md-12">
                 <div class="jumbotron">
-                    <h1 class="display-4">Hola, <?php echo $_SESSION['nombrePaciente']?>!</h1>
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-header">
-                            Tus datos
+                    <div class="container mb-3">
+                    <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                        <h1 class="display-4 text-center mt-5 mb-5">Hola, <?php echo $_SESSION['nombrePaciente']?>!</h1>
+                        <p>A continuacion le mostramos las opciones que puede consultar</p>
+                            <br>
+                            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                                <div class="container-fluid">
+                                    <div class="collapse navbar-collapse" id="navbarText">
+                                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="citasVigentes.php">Ver citas</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="citas_calificar.php">Calificar citas</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="citas_calificar.php">Mi expediente</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="mailto:hospitallasalud01@gmail.com?Subject=Solicitar%20una%20cita">Pedir cita</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </nav>
                         </div>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Correo: <?php echo $_SESSION['correo']?></li>
-                            <li class="list-group-item">Nombre: <?php echo $_SESSION['nombrePaciente']?></li>
-                            <li class="list-group-item">Apellido: <?php echo $_SESSION['apellidoPaciente']?></li>
-                            <li class="list-group-item">DUI: <?php echo $_SESSION['duiPaciente']?></li>
-                            <li class="list-group-item">
-                                Genero: 
-                                <?php 
-                                    if($_SESSION['sexo'] == "M"){
-                                        echo "Masculino";
-                                    }else{
-                                        echo "Femenino";
-                                    }
-                                ?>
-                            </li>
-                            <li class="list-group-item">Edad: <?php echo obtener_edad_segun_fecha(substr($_SESSION['fnacimiento'], 0, 10))?></li>
-                        </ul>
-                    </div>
-                    <hr class="my-4">
-                    <p>A continuacion le mostramos las opciones que puede consultar</p>
-                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                        <div class="container-fluid">
-                            <div class="collapse navbar-collapse" id="navbarText">
-                                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="citasVigentes.php">Ver citas</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="citas_calificar.php">Calificar citas</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="citas_calificar.php">Mi expediente</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="mailto:hospitallasalud01@gmail.com?Subject=Solicitar%20una%20cita">Pedir cita</a>
-                                    </li>
-                                </ul>
+                        <div class="col-md-6 mt-5">
+                            <div class="profile-card-4 text-center">
+                                <img src="https://512pixels.net/downloads/macos-wallpapers-thumbs/10-14-Night-Thumb.jpg" class="img img-responsive w-100">
+                                <div class="profile-content">
+                                <div class="profile-name">
+                                    <?php echo $_SESSION['nombrePaciente']?> <?php echo $_SESSION['apellidoPaciente']?>
+                                    <p>Paciente de Hospital la salud</p>
+                                </div>
+                                <div class="row">
+                                    <div class="col-xs-4">
+                                        <div class="profile-overview">
+                                            <p>Correo</p>
+                                            <h6><?php echo $_SESSION['correo']?></h6>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-4">
+                                        <div class="profile-overview">
+                                            <p>DUI</p>
+                                            <h6><?php echo $_SESSION['duiPaciente']?></h6>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="profile-overview">
+                                            <p>Genero</p>
+                                            <h6>
+                                                <?php 
+                                                    if($_SESSION['sexo'] == "M"){
+                                                        echo "Masculino";
+                                                    }else{
+                                                        echo "Femenino";
+                                                    }
+                                                ?>
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="profile-overview">
+                                            <p>Edad</p>
+                                            <h6><?php echo obtener_edad_segun_fecha(substr($_SESSION['fnacimiento'], 0, 10))?></h6>
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
                             </div>
                         </div>
-                    </nav>
+                    </div>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
